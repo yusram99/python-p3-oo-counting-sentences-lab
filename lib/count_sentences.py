ead9fc837d89660b9f0d9e36845e4b9c44f96070
Your Name <youremail@example.com>
2023-08-27 10:53:02 +0300

    commit message

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            raise ValueError("Value must be a string")
+    
+    @property
+    def value(self):
+        return self._value
+    
+    def is_sentence(self):
+        return self._value.endswith('.')
+    
+    def is_question(self):
+        return self._value.endswith('?')
+    
+    def is_exclamation(self):
+        return self._value.endswith('!')
+    
+    def count_sentences(self):
+        modified_value = self._value.replace('.', '. ').replace('?', '? ').replace('!', '! ')
+        segments = modified_value.split()
+        return len([segment for segment in segments if segment.strip()])
+
+      
--- lib/testing/count_sentences_test.py
+++ lib/testing/count_sentences_test.py
@@ -10,7 +10,7 @@ class TestMyString:
 
     def test_is_class(self):
         '''is a class with the name "MyString".'''
-        string = MyString()
+        string = MyString("Hello, world!")
         assert(type(string) == MyString)
 
     def test_value_string(self):
@@ -40,8 +40,9 @@ class TestMyString:
     def test_count_sentences(self):
         '''returns the number of sentences in the value.'''
         simple_string = MyString("one. two. three?")
-        empty_string = MyString()
+        empty_string = MyString("")
         complex_string = MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
         assert(simple_string.count_sentences() == 3)
         assert(empty_string.count_sentences() == 0)
         assert(complex_string.count_sentences() == 4)
+  
